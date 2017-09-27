f646fe075009b06845be43ac4523690f9e413cfd
Stephanie McIlroy <stephanie.mcilroy@gmail.com>
2017-09-27 11:54:36 -0400

    started coding

---
+++ public/app/directive.js
@@ -0,0 +1,25 @@
+var app=angular.module('healthApp');
+
+app.directive('dashboard', function(){
+
+return {
+    replace: false,
+    restrict: 'E',
+    templateUrl: 'partials/dashboard.html' 
+}
+
+});
+
+app.directive('sidebar', function(){
+    
+    return {
+        replace: false,
+        restrict: 'E',
+        templateUrl: 'partials/sidebar.html' 
+    }
+    
+});
+
+
+
+
