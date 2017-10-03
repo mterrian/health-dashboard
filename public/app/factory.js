139f17128bfb9ea75ecf0f6085e65bbbca1b54b1
knaughton <kevinjamesnaughton@gmail.com>
2017-10-03 09:18:41 -0400

    progress bar style

--- public/app/factory.js
+++ public/app/factory.js
@@ -2,23 +2,5 @@ var app = angular.module('healthApp');
 
 app.factory('healthFactory', function($http, $location) {
 
-
-    // return {
-    //     chartCtrl: chartCtrl,
-    // }
-
-    // function chartCtrl(){
-    //     var chart = document.getElementById("newChart");
-    //     var width = 1
-    //     var id = setInterval(frame, 100);
-    //     function frame(){
-    //         if (width >= 50) {
-    //             clearInterval(id);
-    //         } else {
-    //             width++;
-    //             chart.style.width = width + '%'  
-    //         }
-    //     }
-    // }
     
 });
