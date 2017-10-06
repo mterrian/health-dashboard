7ce09395fa3c2ca5a434b4355a375a6922c4c6d9
Jd Fiscus <jonathan.fiscus@accenture.com>
2017-10-06 18:00:12 -0400

    FitBit: oAuth returned

--- routes.js
+++ routes.js
@@ -1,61 +1,45 @@
-var express = require('express');
-var route = express.Router();
+const express = require('express');
+const route = express.Router();
 const moment = require('moment');
+const FitbitClient = require('fitbit-client-oauth2');
 
-const date = moment().format('dddd, MMMM Do YYYY');
-console.log("Hi", date);
-
-
+const client = new FitbitClient(
+    process.env.FITBIT_CLIENT_ID,
+    process.env.FITBIT_APP_SECRET
+);
 
 route.get('/time', function(req, res) {
-  // console.log(moment);
-  res.json(moment().format('dddd, MMMM Do YYYY'));
+    res.json(moment().format('dddd, MMMM Do YYYY'));
 });
 
-// route.get('/oauth/callback', function(req, res) {
-//   console.log(res);
-//   // parse query for code
-//   res.json(res);
-// });
-
-
-var FitbitClient = require('fitbit-client-oauth2');
-var client = new FitbitClient( '228N9H', '619485e58325cb3d44084ec31d41087b' );
-var redirect_uri = 'http://localhost:4000/oauth/callback';
-var scope = 'activity nutrition profile settings sleep social weight heartrate';
+const scope =
+    'activity nutrition profile settings sleep social weight heartrate';
 
 route.get('/oauth/fitbit', function(req, res, next) {
+    let authorization_uri = client.getAuthorizationUrl(
+        process.env.FITBIT_CALLBACK_URL,
+        scope
+    );
 
-  var authorization_uri = client.getAuthorizationUrl(redirect_uri, scope);
-
-  res.redirect(authorization_uri);
+    res.redirect(authorization_uri);
 });
 
-// If /auth/fitbit/callbac is your redirec_uri
+route.get('/oauth/fitbit/callback', function(req, res, next) {
+    var code = req.query.code;
 
-route.get('/oauth/callback', function(req, res, next) {
+    client
+        .getToken(code, process.env.FITBIT_CALLBACK_URL)
+        .then(function(oauth) {
+            console.log(oauth.token.access_token);
+            // ... save your token on db or session...
 
-  var code = req.query.code;
-
-  client.getToken(code, redirect_uri)
-    .then(function(oauth) {
-      console.log(oauth.token.access_token)
-      // ... save your token on db or session...
-
-      // then redirect
-      res.redirect(302, '/#!/dashboard');
-
-    })
-    .catch(function(err) {
-      // something went wrong.
-      res.json(err)
-
-    });
-
+            // then redirect
+            res.redirect(302, '/#!/dashboard');
+        })
+        .catch(function(err) {
+            // something went wrong.
+            res.json(err);
+        });
 });
-
-
-
-
 
 module.exports = route;
