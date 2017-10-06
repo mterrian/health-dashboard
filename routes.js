af676b94df1344aca749a9ef46fd58b37696754c
Jd Fiscus <jonathan.fiscus@accenture.com>
2017-10-06 15:22:22 -0400

    FitBit: Init

--- routes.js
+++ routes.js
@@ -3,15 +3,58 @@ var route = express.Router();
 const moment = require('moment');
 
 const date = moment().format('dddd, MMMM Do YYYY');
-console.log("Hi",date); 
+console.log("Hi", date);
 
-  
-  
-  route.get('/time', function(req, res) {
-        // console.log(moment);
-          res.json(moment().format('dddd, MMMM Do YYYY'));
-        });
-    
+
+
+route.get('/time', function(req, res) {
+  // console.log(moment);
+  res.json(moment().format('dddd, MMMM Do YYYY'));
+});
+
+// route.get('/oauth/callback', function(req, res) {
+//   console.log(res);
+//   // parse query for code
+//   res.json(res);
+// });
+
+
+var FitbitClient = require('fitbit-client-oauth2');
+var client = new FitbitClient( '228N9H', '619485e58325cb3d44084ec31d41087b' );
+var redirect_uri = 'http://localhost:4000/oauth/callback';
+var scope = 'activity nutrition profile settings sleep social weight heartrate';
+
+route.get('/oauth/fitbit', function(req, res, next) {
+
+  var authorization_uri = client.getAuthorizationUrl(redirect_uri, scope);
+
+  res.redirect(authorization_uri);
+});
+
+// If /auth/fitbit/callbac is your redirec_uri
+
+route.get('/oauth/callback', function(req, res, next) {
+
+  var code = req.query.code;
+
+  client.getToken(code, redirect_uri)
+    .then(function(oauth) {
+      console.log(oauth.token.access_token)
+      // ... save your token on db or session...
+
+      // then redirect
+      res.redirect(302, '/#!/dashboard');
+
+    })
+    .catch(function(err) {
+      // something went wrong.
+      res.json(err)
+
+    });
+
+});
+
+
 
 
 
